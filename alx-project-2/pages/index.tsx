1b62fd0b5fe4eb31948bd46ce8d80d147c1ac151
4honore <ishimwehonore450@gmail.com>
2025-06-04 12:34:11 +0200

    some changes in pages/index

--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import Header from "@/components/layout/Header";
 
-export default function index() {
-  return <div>Home page</div>
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
 }
